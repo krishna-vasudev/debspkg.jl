63e78af5a5e8fb71264b4888414a4a5bf4ab6234
Debraj Bhal <debrajisothermal@gmail.com>
2021-01-10 20:24:51 +0530

    added tests for function f

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using debspkg
 using Test
 
+@test f(3,2)==25
+
 @testset "debspkg.jl" begin
     # Write your tests here.
+    @test f(3,2)==25
+    @test f(2,2)==16
+    @test f(3,1)==16
+    @test f(1,1)==4
+    @test f(0,0)==0
 end
