dd788f253bd965bdadd0c28cf22c37f18011bf6e
Debraj Bhal <debrajisothermal@gmail.com>
2021-01-17 19:58:15 +0530

    added documentation and docstrings

--- docs/src/index.md
+++ docs/src/index.md
@@ -3,6 +3,10 @@ CurrentModule = debspkg
 ```
 
 # debspkg
+This is a package developed to calculate a function and test
+**documentor.jl** features.
+
+These helps a lot.
 
 ```@index
 ```
--- src/extra_file.jl
+++ src/extra_file.jl
@@ -1,2 +1,5 @@
+"""This is a function to calcuate (x+y)*(x+y)"""
 f(x,y)=x^2+2*x*y+y^2
+"""This calculates the derivative with respect to x of the
+function f(x,y)"""
 derivative_of_f1(x,y)=ForwardDiff.derivative(x->f(x,y),x)
